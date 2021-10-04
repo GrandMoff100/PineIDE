924f31000cf6bba36c4e49f367ee67b24d87c603
GrandMoff100 <minecraftcrusher100@gmail.com>
2021-10-04 18:08:30 +0000

    Added sidebar layout.

--- pineide/ide.py
+++ pineide/ide.py
@@ -6,16 +6,19 @@ from rich.traceback import Traceback
 
 from textual.app import App
 from textual.widgets import Footer, FileClick, ScrollView, DirectoryTree
+from textual.reactive import Reactive, watch
 from .custom_widgets import CustomHeader
 
 
 class PineIDE(App):
     """The PineIDE app class"""
-
+    states = Reactive({
+        'show_file_sidebar': True
+    })
     async def on_load(self) -> None:
         """Sent before going in to application mode."""
 
-        await self.bind("b", "view.toggle('sidebar')", "Toggle File View")
+        await self.bind("b", "toggle_sidebar", "Toggle File View")
         await self.bind("q", "quit", "Quit")
         
         self.path = abspath(join(basename(__file__), "../../../"))
@@ -33,9 +36,16 @@ class PineIDE(App):
             ScrollView(self.directory),
             edge="left",
             size=48,
-            name="sidebar"
+            name="sidebar",
+            z=1
         )
         await self.view.dock(self.body, edge="top")
+
+    async def action_toggle_sidebar(self):
+        self.states.update(show_toggle_sidebar=not self.states.get('show_toggle_sidebar'))
+
+    async def update_sidebar(self, states: dict):
+        self.bar.animate("layout_offset_x", 0 if states.get('show_toggle_sidebar') else -48)
 
     async def handle_file_click(self, message: FileClick) -> None:
         """A message sent by the directory tree when a file is clicked."""
--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
     author='GrandMoff100',
     author_email='nlarsen23.student@gmail.com',
     description='An interactive Python IDE in your terminal',
-    install_requires=['textual', 'click', 'rich']
+    install_requires=['textual', 'click', 'rich'],
+    entry_points={
+        'console_scripts': ['pine = pineide.cli:cli']
+    }
 )
