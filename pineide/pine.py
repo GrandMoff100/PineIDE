6d7d61691e09abef26afd91f54077d2a6258aca4
Nate <nlarsen23.student@gmail.com>
2023-05-13 15:48:57 -0500

    Establish layout

--- pineide/pine.py
+++ pineide/pine.py
@@ -1,64 +1,74 @@
-from rich.syntax import Syntax
-from rich.traceback import Traceback
+from pathlib import Path
+from typing import Iterable
 
 from textual import events
-from textual.app import App, ComposeResult
-from textual.containers import Container, VerticalScroll
-from textual.reactive import var as Variable
-from textual.widgets import DirectoryTree, Footer, Header, Static
+from textual.app import App, ComposeResult, RenderResult
+from textual.containers import Horizontal, Vertical, Container
+from textual.reactive import Reactive
+from textual.widgets import Footer, Static, DirectoryTree
+from textual.widget import Widget
+
+from pineide.header import PineHeader
+
+
+class Terminal(Static):
+    def compose(self) -> ComposeResult:
+        yield Static("Terminal")
+
+
+class FileTabs(Static):
+    def compose(self) -> ComposeResult:
+        yield Static("example.py")
+
+
+class SideButtons(Static):
+    def compose(self) -> ComposeResult:
+        with Vertical():
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+            yield Static("📄")
+
+
+class Sidebar(Static):
+    def compose(self) -> ComposeResult:
+        yield DirectoryTree(path=self.app.path)
+
+    def _on_mount(self, _: events.Mount) -> None:
+        tree = self.query_one("DirectoryTree")
+        tree.show_root = False
+        tree.show_guides = False
+        tree.guide_depth = 1
 
 
 class Pine(App):
     CSS_PATH = "pine.css"
     BINDINGS = [
-        ("f", "toggle_files", "Toggle Files"),
         ("q", "quit", "Quit"),
     ]
 
-    show_tree = Variable(True)
+    icon = Reactive("")
 
-    def __init__(self, path: str) -> None:
-        super().__init__()
-        self._path = path
-
-    def watch_show_tree(self, show_tree: bool) -> None:
-        """Called when show_tree is modified."""
-        self.set_class(show_tree, "-show-tree")
+    def __init__(self, *, path: str, **kwargs):
+        super().__init__(**kwargs)
+        self.path = Path(path).absolute()
 
     def compose(self) -> ComposeResult:
-        """Compose our UI."""
-        yield Header()
-        with Container():
-            yield DirectoryTree(self._path, id="tree-view")
-            with VerticalScroll(id="code-view"):
-                yield Static(id="code", expand=True)
+        self.title = "PineIDE"
+        self.sub_title = self.path.as_posix()
+        self.icon = "🌲"
+        yield PineHeader(id="header")
+        with Horizontal():
+            yield SideButtons()
+            yield Sidebar()
+            with Container():
+                yield FileTabs(id="file-tabs")
+                yield Terminal(id="terminal")
         yield Footer()
-
-    def on_mount(self, _: events.Mount) -> None:
-        self.query_one("#tree-view").focus()
-
-    def on_directory_tree_file_selected(
-        self, event: DirectoryTree.FileSelected
-    ) -> None:
-        """Called when the user click a file in the directory tree."""
-        event.stop()
-        code_view = self.query_one("#code", Static)
-        try:
-            syntax = Syntax.from_path(
-                str(event.path),
-                line_numbers=True,
-                word_wrap=False,
-                indent_guides=True,
-                theme="github-dark",
-            )
-        except Exception:
-            code_view.update(Traceback(theme="github-dark", width=None))
-            self.sub_title = "ERROR"
-        else:
-            code_view.update(syntax)
-            self.query_one("#code-view").scroll_home(animate=False)
-            self.sub_title = str(event.path)
-
-    def action_toggle_files(self) -> None:
-        """Called in response to key binding."""
-        self.show_tree = not self.show_tree
